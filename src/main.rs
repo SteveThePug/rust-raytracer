eb5d866ade5b2bd92fce83042a042942087a7e5a
STP <adam.a.french@outlook.com>
2023-11-10 10:23:11 -0500

    webgl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-fn main() {
-    println!("Hello world");
-}
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
+fn main() {
+    env_logger::init(); // Necessary for logging within WGPU
+    let event_loop = EventLoop::new(); // Loop provided by winit for handling window events
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // Opens the window and starts processing events (although no events are handled yet)
+    event_loop.run(move |event, _, control_flow| {});
+}
