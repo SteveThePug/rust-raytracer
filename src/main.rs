fefa7830d1028eb5aa9b722eb7d60576db0cdee9
STP <adam.a.french@outlook.com>
2023-11-11 00:27:26 -0500

    Started using lin alg library

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-pub mod vec4;
+#![allow(dead_code)]
+mod ray;
+use nalgebra::{Matrix4, Vector4};
+
+use ray::Ray;
 
 fn main() {
-    println!("Hello world");
+    let x = Vector4::new(1.0, 1.0, 1.0, 1.0);
+    let mat_a = Matrix4::from_diagonal_element(0.12);
+    println!("mat_a*x = {}", { mat_a * x });
 }
-
