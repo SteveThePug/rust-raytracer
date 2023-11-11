246bc697baf0098007f0fced2e679edd8d061d70
STP <adam.a.french@outlook.com>
2023-11-10 23:33:33 -0500

    creating startup environment

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+pub mod vec4;
 
 fn main() {
-    env_logger::init(); // Necessary for logging within WGPU
-    let event_loop = EventLoop::new(); // Loop provided by winit for handling window events
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    // Opens the window and starts processing events (although no events are handled yet)
-    event_loop.run(move |event, _, control_flow| {});
+    println!("Hello world");
 }
+
