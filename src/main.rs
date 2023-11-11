24c7e719b07c9d34859ff45d40b3f9301e9587b9
STP <adam.a.french@outlook.com>
2023-11-11 00:32:03 -0500

    added winit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #![allow(dead_code)]
 mod ray;
 use nalgebra::{Matrix4, Vector4};
-
 use ray::Ray;
+use std::borrow::Cow;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::Window,
+};
 
 fn main() {
     let x = Vector4::new(1.0, 1.0, 1.0, 1.0);
