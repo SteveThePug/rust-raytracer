9c863dc8bf688f7b990b351d72b5aa1259072b0c
STP <adam.a.french@outlook.com>
2023-11-12 03:47:03 -0500

    Added sphere, circle, cone

--- src/ray.rs
+++ src/ray.rs
@@ -11,7 +11,7 @@ impl Ray {
     pub fn new(_a: Point3<f32>, _b: Vector3<f32>) -> Ray {
         Ray { a: _a, b: _b }
     }
-    pub fn at_t(self, t: f32) -> Point3<f32> {
+    pub fn at_t(&self, t: f32) -> Point3<f32> {
         self.a + self.b * t
     }
 }
